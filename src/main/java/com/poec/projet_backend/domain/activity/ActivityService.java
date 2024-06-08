00da19d85f3e73de69ec5da627a31ec2d6e024d0
yoyodes1000 <yoyodes1000@gmail.com>
2024-06-08 11:47:21 +0200

    feat: activity/add ok

--- src/main/java/com/poec/projet_backend/domain/activity/ActivityService.java
+++ src/main/java/com/poec/projet_backend/domain/activity/ActivityService.java
@@ -1,5 +1,11 @@
 package com.poec.projet_backend.domain.activity;
 
+import com.poec.projet_backend.domain.booking.BookingRepository;
+import com.poec.projet_backend.domain.category.CategoryRepository;
+import com.poec.projet_backend.domain.city.CityRepository;
+import com.poec.projet_backend.domain.department.Department;
+import com.poec.projet_backend.domain.department.DepartmentRepository;
+import com.poec.projet_backend.domain.region.RegionRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +16,16 @@ public class ActivityService {
 
     @Autowired
     private ActivityRepository repository;
+    @Autowired
+    private CityRepository cityRepository;
+    @Autowired
+    private DepartmentRepository departmentRepository;
+    @Autowired
+    private RegionRepository regionRepository;
+    @Autowired
+    private BookingRepository bookingRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public List<Activity> getAll() {
         return repository.findAll();
@@ -22,8 +38,14 @@ public class ActivityService {
                 );
     }
 
-    public Activity add(Activity activity) {
-        return repository.save(activity);
+    public Activity add(ActivityFrontToBackDTO activityFrontToBackDTO) {
+        Activity newActivity = ActivityFrontToBackDTO.mapToEntity(activityFrontToBackDTO);
+        newActivity.setCity(cityRepository.findById(activityFrontToBackDTO.cityId()));
+        newActivity.setDepartment(departmentRepository.findById(activityFrontToBackDTO.departmentId()));
+        newActivity.setRegion(regionRepository.findById(activityFrontToBackDTO.regionId()));
+        newActivity.setCategories(categoryRepository.findAllById(activityFrontToBackDTO.categoryIds()));
+        newActivity.setBookings(bookingRepository.findAllById(activityFrontToBackDTO.bookingIds()));
+        return repository.save(newActivity);
     }
 
     public Activity update(Activity activity, Long id) {
--- src/main/java/com/poec/projet_backend/domain/city/CityRepository.java
+++ src/main/java/com/poec/projet_backend/domain/city/CityRepository.java
@@ -2,5 +2,8 @@ package com.poec.projet_backend.domain.city;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CityRepository extends JpaRepository<City, Long> {
+    City findById(Optional<Long> aLong);
 }
