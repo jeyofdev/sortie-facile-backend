ee5f42974595504b0586e37b88c99a0ce0af393f
jeyofdev <jgregoire.dev@gmail.com>
2024-06-16 09:44:18 +0200

    feat : add city to ActivityDTO

--- src/main/java/com/poec/projet_backend/domain/activity/ActivityDTO.java
+++ src/main/java/com/poec/projet_backend/domain/activity/ActivityDTO.java
@@ -11,35 +11,37 @@ public record ActivityDTO(
         String description,
         int nbGuest,
         String hour,
-        boolean isVisible
+        boolean isVisible,
+        Long cityId
 ) {
 
     public static ActivityDTO mapFromEntity(Activity activity) {
         return new ActivityDTO(
-            activity.getId(),
-            activity.getName(),
-            activity.getDate(),
-            activity.getAge(),
-            activity.getImgUrl(),
-            activity.getLink(),
-            activity.getDescription(),
-            activity.getNbGuest(),
-            activity.getHour(),
-            activity.isVisible()
+                activity.getId(),
+                activity.getName(),
+                activity.getDate(),
+                activity.getAge(),
+                activity.getImgUrl(),
+                activity.getLink(),
+                activity.getDescription(),
+                activity.getNbGuest(),
+                activity.getHour(),
+                activity.isVisible(),
+                activity.getCity().getId()
         );
     }
 
     public static Activity mapToEntity(ActivityDTO activityDTO) {
         return new Activity(
-            activityDTO.name(),
-            activityDTO.date(),
-            activityDTO.age(),
-            activityDTO.imgUrl(),
-            activityDTO.link(),
-            activityDTO.description(),
-            activityDTO.nbGuest(),
-            activityDTO.hour(),
-            activityDTO.isVisible()
+                activityDTO.name(),
+                activityDTO.date(),
+                activityDTO.age(),
+                activityDTO.imgUrl(),
+                activityDTO.link(),
+                activityDTO.description(),
+                activityDTO.nbGuest(),
+                activityDTO.hour(),
+                activityDTO.isVisible()
         );
     }
 }
