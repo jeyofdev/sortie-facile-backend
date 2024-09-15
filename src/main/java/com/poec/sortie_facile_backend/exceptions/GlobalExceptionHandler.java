fec7f554fef60dabfcce2dc5f1cf099bd05440ea
jeyofdev <jgregoire.dev@gmail.com>
2024-09-15 17:29:51 +0200

    feat : handle global exceptions

--- src/main/java/com/poec/sortie_facile_backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/poec/sortie_facile_backend/exceptions/GlobalExceptionHandler.java
@@ -4,68 +4,112 @@ import com.poec.sortie_facile_backend.exceptions.model.ErrorResponse;
 import com.poec.sortie_facile_backend.util.Helper;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * to handle the case when a NotFoundException is thrown
+     */
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException exception, HttpServletRequest request) {
-        return handleException(exception, HttpStatus.NOT_FOUND, request);
+        return handleException(exception, HttpStatus.NOT_FOUND, request, null);
     }
 
+    /**
+     * to handle the case when the user is not found.
+     */
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUsernameNotFoundException(UsernameNotFoundException exception, HttpServletRequest request) {
-        return handleException(exception, HttpStatus.NOT_FOUND, request);
+        return handleException(exception, HttpStatus.NOT_FOUND, request, null);
     }
 
+    /**
+     * to handle the case when the requested entity is not found.
+     */
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException exception, HttpServletRequest request) {
-        return handleException(exception, HttpStatus.NOT_FOUND, request);
+        return handleException(exception, HttpStatus.NOT_FOUND, request, null);
     }
 
+    /**
+     * to handle the case when the provided username already exists in the database.
+     */
     @ExceptionHandler(UsernameAlreadyTakenException.class)
     public ResponseEntity<ErrorResponse> handleUsernameAlreadyTakenException(UsernameAlreadyTakenException exception, HttpServletRequest request) {
-        return handleException(exception, HttpStatus.NOT_FOUND, request);
+        return handleException(exception, HttpStatus.NOT_FOUND, request, null);
     }
 
+    /**
+     * to handle the case when the provided username or password are incorrect.
+     */
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException exception, HttpServletRequest request) {
-        return handleException(exception, HttpStatus.NOT_FOUND, request);
+        return handleException(exception, HttpStatus.NOT_FOUND, request, null);
     }
 
+    /**
+     * to handle the case when the user does not have the necessary roles
+     * to access the requested resource.
+     */
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException exception, HttpServletRequest request) {
-        return handleException(exception, HttpStatus.NOT_FOUND, request);
+        return handleException(exception, HttpStatus.NOT_FOUND, request, null);
     }
 
-    /*@ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException exception) {
-        Map<String, String> errors = new HashMap<>();
+    /**
+     * to handle the case when the incoming request does not match the expected type.
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception, HttpServletRequest request) {
+        return handleException(
+                exception,
+                HttpStatus.BAD_REQUEST,
+                request,
+                String.format(
+                        "The parameter '%s' of value '%s' could not be converted to type '%s'",
+                        exception.getName(),
+                        exception.getValue(),
+                        Objects.requireNonNull(exception.getRequiredType()).getSimpleName()
+                )
+        );
+    }
 
-        exception.getConstraintViolations().forEach(violation -> {
-            String fieldName = violation.getPropertyPath().toString();
-            String errorMessage = violation.getMessage();
-            errors.put(fieldName, errorMessage);
-        });
+    /**
+     * to handle the case when trying to access an object that does not exist
+     * missing or malformed
+     */
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception, HttpServletRequest request) {
+        String message = "";
 
-        errors.put("validate", String.valueOf(exception.getConstraintViolations().isEmpty()));
+        // Check if the error is for a missing request body
+        if (exception.getMessage() != null && exception.getMessage().contains("Required request body is missing")) {
+            message = "Request body is missing or malformed";
+        } else {
+            message = "Malformed JSON request: " + exception.getMessage();
+        }
 
-        return new ResponseEntity<>(errors, HttpStatus.UNPROCESSABLE_ENTITY);
-    }*/
+        return handleException(exception, HttpStatus.BAD_REQUEST, request, message);
+    }
 
+    /**
+     * to handle the case when invalid or inappropriate argument.
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         Map<String, String> errors = new HashMap<>();
@@ -74,22 +118,26 @@ public class GlobalExceptionHandler {
             String fieldName = error.getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
+            errors.put("validate", "false");
         });
 
         return new ResponseEntity<>(errors, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
+    /**
+     * Others
+     */
     @ExceptionHandler
     public ResponseEntity<Map<String, String>> handleException(Exception exception) {
         return handleException(exception, HttpStatus.BAD_REQUEST);
     }
 
-    private ResponseEntity<ErrorResponse> handleException(Exception exception, HttpStatus status, HttpServletRequest request) {
+    private ResponseEntity<ErrorResponse> handleException(Exception exception, HttpStatus status, HttpServletRequest request, String message) {
         exception.printStackTrace();
 
         ErrorResponse errorResponse = new ErrorResponse();
 
-        errorResponse.setMessage(exception.getMessage());
+        errorResponse.setMessage(message != null ? message : exception.getMessage());
         errorResponse.setStatus(status.value());
         errorResponse.setExceptionName(exception.getClass().getSimpleName());
         errorResponse.setDate(Helper.simpleDateFormat());
