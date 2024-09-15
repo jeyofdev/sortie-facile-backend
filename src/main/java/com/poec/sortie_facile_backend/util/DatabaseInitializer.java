f8f0ee2806b1b13d26ba9f7d1954c7e4fd2d7bae
jeyofdev <jgregoire.dev@gmail.com>
2024-09-15 18:44:13 +0200

    feat : init some region, department and city with dataInitializer

--- src/main/java/com/poec/sortie_facile_backend/util/DatabaseInitializer.java
+++ src/main/java/com/poec/sortie_facile_backend/util/DatabaseInitializer.java
@@ -7,10 +7,22 @@ import com.poec.sortie_facile_backend.core.enums.Role;
 import com.poec.sortie_facile_backend.auth_user.AuthUser;
 import com.poec.sortie_facile_backend.auth_user.AuthUserRepository;
 import com.poec.sortie_facile_backend.domain.category.dto.SaveCategoryDTO;
+import com.poec.sortie_facile_backend.domain.city.City;
+import com.poec.sortie_facile_backend.domain.city.CityMapper;
+import com.poec.sortie_facile_backend.domain.city.CityRepository;
+import com.poec.sortie_facile_backend.domain.city.dto.SaveCityDTO;
 import com.poec.sortie_facile_backend.domain.contact.Contact;
 import com.poec.sortie_facile_backend.domain.contact.ContactMapper;
 import com.poec.sortie_facile_backend.domain.contact.ContactRepository;
 import com.poec.sortie_facile_backend.domain.contact.dto.SaveContactDTO;
+import com.poec.sortie_facile_backend.domain.department.Department;
+import com.poec.sortie_facile_backend.domain.department.DepartmentMapper;
+import com.poec.sortie_facile_backend.domain.department.DepartmentRepository;
+import com.poec.sortie_facile_backend.domain.department.dto.SaveDepartmentDTO;
+import com.poec.sortie_facile_backend.domain.region.Region;
+import com.poec.sortie_facile_backend.domain.region.RegionMapper;
+import com.poec.sortie_facile_backend.domain.region.RegionRepository;
+import com.poec.sortie_facile_backend.domain.region.dto.SaveRegionDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,9 +40,15 @@ public class DatabaseInitializer implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
     private final ContactRepository contactRepository;
+    private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
+    private final CityRepository cityRepository;
 
     private final CategoryMapper categoryMapper;
     private final ContactMapper contactMapper;
+    private final RegionMapper regionMapper;
+    private final DepartmentMapper departmentMapper;
+    private final CityMapper cityMapper;
 
     @Override
     public void run(String... args) throws Exception {
@@ -67,16 +85,19 @@ public class DatabaseInitializer implements CommandLineRunner {
     private void createDatas() {
         this.createMessageEmails();
         this.createCategories();
+        this.createRegions();
+        this.createDepartments();
+        this.createCities();
     }
 
     private void createCategories() {
-        SaveCategoryDTO saveCategorySport = new SaveCategoryDTO("Sport", "https://images.unsplash.com/photo-1461896836934-ffe607ba8211?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BvcnR8ZW58MHx8MHx8fDA%3D");
-        SaveCategoryDTO saveCategoryMovie = new SaveCategoryDTO("Cinéma", "https://images.unsplash.com/photo-1604061986761-d9d0cc41b0d1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8VGFibGUlMjBCYXNzZXxlbnwwfHwwfHx8MA%3D%3D");
-        SaveCategoryDTO saveCategoryCulture = new SaveCategoryDTO("Culture", "https://plus.unsplash.com/premium_photo-1661407582641-9ce38a3c8402?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Q2FuYXAlQzMlQTl8ZW58MHx8MHx8fDA%3D");
-        SaveCategoryDTO saveCategoryMusic = new SaveCategoryDTO("Musique", "https://images.unsplash.com/photo-1595428774223-ef52624120d2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8QXJtb2lyZXxlbnwwfHwwfHx8MA%3D%3D");
-        SaveCategoryDTO saveCategoryVideoGame = new SaveCategoryDTO("Jeux vidéos", "https://images.unsplash.com/photo-1586753513812-462ed2a82584?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fCVDMyVBOWNsYWlyYWdlJTIwbGVkfGVufDB8fDB8fHww");
-
-        List<SaveCategoryDTO> saveCategoryList = Arrays.asList(saveCategorySport, saveCategoryMovie, saveCategoryCulture, saveCategoryMusic, saveCategoryVideoGame);
+        List<SaveCategoryDTO> saveCategoryList = Arrays.asList(
+                new SaveCategoryDTO("Sport", "https://images.unsplash.com/photo-1461896836934-ffe607ba8211?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BvcnR8ZW58MHx8MHx8fDA%3D"),
+                new SaveCategoryDTO("Cinéma", "https://images.unsplash.com/photo-1604061986761-d9d0cc41b0d1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8VGFibGUlMjBCYXNzZXxlbnwwfHwwfHx8MA%3D%3D"),
+                new SaveCategoryDTO("Culture", "https://plus.unsplash.com/premium_photo-1661407582641-9ce38a3c8402?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Q2FuYXAlQzMlQTl8ZW58MHx8MHx8fDA%3D"),
+                new SaveCategoryDTO("Musique", "https://images.unsplash.com/photo-1595428774223-ef52624120d2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8QXJtb2lyZXxlbnwwfHwwfHx8MA%3D%3D"),
+                new SaveCategoryDTO("Jeux vidéos", "https://images.unsplash.com/photo-1586753513812-462ed2a82584?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fCVDMyVBOWNsYWlyYWdlJTIwbGVkfGVufDB8fDB8fHww")
+        );
 
         for (SaveCategoryDTO saveCategory : saveCategoryList) {
             Category category = categoryMapper.mapToEntity(saveCategory);
@@ -85,14 +106,62 @@ public class DatabaseInitializer implements CommandLineRunner {
     }
 
     private void createMessageEmails() {
-        SaveContactDTO saveEmailMessageA = new SaveContactDTO("Renseignement sur une activité", "johndoe@test.com", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in felis quis odio elementum bibendum a id tellus. Integer ultricies vel mauris eu pretium. Donec efficitur felis quis tincidunt vulputate. Aliquam et odio efficitur, bibendum elit sed, ullamcorper tellus. Pellentesque at sapien vitae diam euismod viverra eu vel nisi.", false);
-        SaveContactDTO saveEmailMessageB = new SaveContactDTO("Annulation de réservation", "janedoe@test.com", "Sed at rhoncus sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer maximus odio id sem tristique efficitur. Sed efficitur, tortor sed tempus aliquet, quam orci varius tellus, eget vestibulum urna justo quis sapien. Phasellus gravida consequat pharetra.", false);
-
-        List<SaveContactDTO> saveEmailMessageList = Arrays.asList(saveEmailMessageA, saveEmailMessageB);
+        List<SaveContactDTO> saveEmailMessageList = Arrays.asList(
+                new SaveContactDTO("Renseignement sur une activité", "johndoe@test.com", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in felis quis odio elementum bibendum a id tellus. Integer ultricies vel mauris eu pretium. Donec efficitur felis quis tincidunt vulputate. Aliquam et odio efficitur, bibendum elit sed, ullamcorper tellus. Pellentesque at sapien vitae diam euismod viverra eu vel nisi.", false),
+                new SaveContactDTO("Annulation de réservation", "janedoe@test.com", "Sed at rhoncus sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer maximus odio id sem tristique efficitur. Sed efficitur, tortor sed tempus aliquet, quam orci varius tellus, eget vestibulum urna justo quis sapien. Phasellus gravida consequat pharetra.", false)
+        );
 
         for (SaveContactDTO saveEmailMessage : saveEmailMessageList) {
             Contact emailMessage = contactMapper.mapToEntity(saveEmailMessage);
             contactRepository.save(emailMessage);
+        }
+    }
+
+    private void createRegions() {
+        List<SaveRegionDTO> saveRegionList = Arrays.asList(
+                new SaveRegionDTO("Nouvelle Aquitaine"),
+                new SaveRegionDTO("Auvergne Rhone Alpes"),
+                new SaveRegionDTO("Ile de France")
+        );
+
+        for (SaveRegionDTO saveRegion : saveRegionList) {
+            Region region = regionMapper.mapToEntity(saveRegion);
+            regionRepository.save(region);
+        }
+    }
+
+    private void createDepartments() {
+        List<SaveDepartmentDTO> saveDepartmentList = Arrays.asList(
+                new SaveDepartmentDTO("Charente", null),
+                new SaveDepartmentDTO("Charente-Maritime", null),
+                new SaveDepartmentDTO("Gironde", null),
+                new SaveDepartmentDTO("Landes", null),
+                new SaveDepartmentDTO("Rhône", null),
+                new SaveDepartmentDTO("Savoie", null)
+        );
+
+        for (SaveDepartmentDTO saveDepartment : saveDepartmentList) {
+            Department department = departmentMapper.mapToEntity(saveDepartment);
+            departmentRepository.save(department);
+        }
+    }
+
+    private void createCities() {
+        List<SaveCityDTO> saveCityList = Arrays.asList(
+                new SaveCityDTO("Bordeaux", "33000", null),
+                new SaveCityDTO("Mérignac", "33700", null),
+                new SaveCityDTO("Le Bouscat", "33110", null),
+                new SaveCityDTO("Bègles", "33130",  null),
+                new SaveCityDTO("Mont-de-Marsan", "40000",null),
+                new SaveCityDTO("Biscarrosse", "40600", null),
+                new SaveCityDTO("Dax", "40100", null),
+                new SaveCityDTO("Lyon", "69001", null),
+                new SaveCityDTO("Villeurbanne", "69100", null)
+        );
+
+        for (SaveCityDTO saveCity : saveCityList) {
+            City department = cityMapper.mapToEntity(saveCity);
+            cityRepository.save(department);
         }
     }
 }
